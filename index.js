36bb7e9f07fb0e26423d423f6a55a92b4e9acd82
Divmrley <nexted.marley@gmail.com>
2022-01-25 19:13:43 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+// object
+
+// const greet = document.querySelector('.myname');
+
+// /* object car 
+//  properties
+//  color of car
+//  size of car
+//  weight of car
+//  name of car
+
+//  methods
+//  car.start()
+// car.drive()
+// car.brake()
+// car.stop()
+// */
+ 
+// const car = {
+//     // properties
+//     name :"benz",
+//     color:'red',
+//     model:"123cw" ,
+//     // methods
+//     start:function () {
+//         return 'i am starting';
+//     },
+//     stop:function () {
+//         return 'i am stoping';
+//     },
+//     brake:function () {
+//         return 'i am brakeing';
+//     },
+
+// }
+
+ 
+
+// console.log(car.start()); 
+// console.log(car.brake()); 
+
+
+
+// Arrays
+
+// const cars = ["Saab", "Volvo", "BMW","tfuiyjtr", "srse", "wre6456"];
+
+// console.log(cars[cars.length-1] ); 
+  
+
+// 
+
+// const items = document.getElementById('demo');
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// delete fruits[0];
+// // items.innerHTML =fruits.toString()
+// // items.innerHTML =fruits.join("#")
+// // items.innerHTML =fruits.pop()
+// // items.innerHTML =fruits.push("kiwi")
+// console.log(fruits);
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+
+// const jj =myGirls.concat(myBoys)
+
+// console.log(jj)
+
+
+
+
+
+// let hisname ="kk"
+
+// console.log(hisname.split(' ')[0][0]);
+
+
+/*
+
+homework
+convert "kofi kumi " to kk
+
+
+
+*/ 
