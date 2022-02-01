a747018b3e30c2e2f1b4ba0f9bc0c7095dc6baf1
Divmrley <nexted.marley@gmail.com>
2022-02-01 19:17:35 +0000

    l

--- index.js
+++ index.js
@@ -80,6 +80,55 @@
 homework
 convert "kofi kumi " to kk
 
-
-
 */ 
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits);
+// console.log(fruits.sort());
+// console.log(fruits.reverse());
+
+
+// let hisname ="kofi sumi" 
+// let m=hisname.split(' ')[0][0]
+// let k=hisname.split(' ')[1][0] 
+
+
+// let  kk=m.concat(k)
+// console.log(kk); 
+
+// Numeric Sort
+// const numb =[40, 100, 1, 5, 25, 10];
+
+// document.getElementById('demo').innerHTML=numb;
+
+
+// numb.sort(function(a,b){return a-b;}) 
+// document.getElementById('demo2').innerHTML=numb;
+
+
+// // let myTxt ="";
+// // numbers.forEach(function(value){
+// //    console.log(1000+'1');
+// //    console.log(1000+'1');
+// // })
+const numbers = [45, 4, 9, 16, 25];
+let kk=numbers.filter(function (val) {
+    return val>12; 
+})
+
+console.log(kk);
+
+
+const movements=[200, 450, -400, 3000, -650, -130, 70, 1300]
+function add(total,value) {
+    return total+value;
+
+}
+// let sum = movements.reduce(add); 
+
+
+
+document.getElementById('demo').innerHTML=sum;
+
+
+console.log(200+ 450+ -400+ 3000+ -650+ -130+ 70+ 1300);
